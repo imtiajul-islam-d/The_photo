b628a93bb36bdca9ee029c1d0f0bd8fe9ba0f4bd
imtiajul-islam-d <imtiajul@hotmail.com>
2022-11-10 18:35:48 +0600

    review section completed

--- src/Pages/ServiceDetails/ReviewForService/ReviewforService.js
+++ src/Pages/ServiceDetails/ReviewForService/ReviewforService.js
@@ -1,17 +1,22 @@
-import React, { useContext, useEffect } from "react";
-import { Link} from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../../../context/AuthContext/AuthProvider";
 import ReviewCard from "./ReviewCard";
 import ReviewInputForm from "./ReviewInputForm.js/ReviewInputForm";
 
 const ReviewforService = ({ id }) => {
   const { user } = useContext(AuthContext);
+  const [reviewDisplay, setReviewDisplay] = useState([]);
+  const [loader, setLoader] = useState(true);
   useEffect(() => {
     const url = `https://personal-review-server.vercel.app/getReview/${id}`;
     fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
-  },[])
+      .then((res) => res.json())
+      .then((data) => {
+        setReviewDisplay(data);
+        setLoader(false);
+      });
+  }, [id]);
   // useEffect(()=> {
   //   const url = `http://localhost:5000/getMyReview/${user.email}`
   //   fetch(url)
@@ -23,13 +28,14 @@ const ReviewforService = ({ id }) => {
     <section className="py-6 sm:py-12 dark:bg-gray-800 dark:text-gray-100">
       <div className="container p-6 mx-auto space-y-8">
         <div className="space-y-2 text-center">
-          <h2 className="text-3xl font-bold">Partem reprimique an pro</h2>
-          <p className="font-serif text-sm dark:text-gray-400">
-            Qualisque erroribus usu at, duo te agam soluta mucius.
-          </p>
+          <h2 className="text-3xl font-bold">Let's see, what people say!</h2>
         </div>
         <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
-          <ReviewCard></ReviewCard>
+          {loader ? (
+            <h2>Loading</h2>
+          ) : (
+            reviewDisplay.data.map((review) => <ReviewCard review={review}></ReviewCard>)
+          )}
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
@@ -45,7 +51,9 @@ const ReviewforService = ({ id }) => {
           ></ReviewInputForm>
         ) : (
           <>
-            <Link className="bg-orange-400 py-2 px-3" to="/login">Login</Link>
+            <Link className="bg-orange-400 py-2 px-3" to="/login">
+              Login
+            </Link>
           </>
         )}
       </div>
