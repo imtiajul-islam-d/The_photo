75d4ab6040ed9e2c92c93e90ab6273c4ffccccd2
imtiajul-islam-d <imtiajul@hotmail.com>
2022-11-11 12:28:48 +0600

    modal added

---
+++ src/Pages/MyReviews/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Modal = ({serviceName, handleUpdate, setCardButton , cardButton}) => {
+    return (
+        <div className='fixed inset-0 bg-opacity-30 backdrop-blur-md z-10 flex justify-center items-center'>
+            <div>
+                <form onSubmit={handleUpdate} className='bg-orange-400 p-5'>
+                    <div className='text-center mb-3'>
+                       {serviceName}
+                    </div>
+                    <div>
+                        <textarea  placeholder='Update your review' style={{resize: "none"}} name="updatedReview" id="updatedReview" cols="30" rows="10" className='text-black'></textarea>
+                    </div>
+                    <div className='space-x-4 mt-2'>
+                        <button onClick={() => setCardButton(!cardButton)} className='bg-white text-black py-2 px-3'>cancel</button>
+                        <button type='submit' className='bg-white text-black py-2 px-3'>update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
--- src/Pages/MyReviews/ReviewCard.js
+++ src/Pages/MyReviews/ReviewCard.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaRegEdit, FaArchive } from "react-icons/fa";
+import Modal from "./Modal";
 
 const ReviewCard = ({ review, handleDelete }) => {
+  const [cardButton, setCardButton] = useState(false)
+  // const [modalButton, setModalButton] = useState(true)
   const { reviewText, _id, userName, userImage, userEmail, serviceName } =
     review;
+
+    const handleUpdate= (e) => {
+      e.preventDefault()
+      setCardButton(!cardButton)
+      console.log(e.target.updatedReview.value);
+    }
   return (
     <div className="container bg-orange-400 text-white flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
       <div className="flex p-4">
         <div className=" w-full items-center">
-          <div className="text-center mb-3 text-2xl font-bold text-white">{serviceName}</div>
+          <div className="text-center mb-3 text-2xl font-bold text-white">
+            {serviceName}
+          </div>
           <div className="flex flex-col justify-center items-center">
             <img
               src={userImage}
@@ -24,14 +35,18 @@ const ReviewCard = ({ review, handleDelete }) => {
       <div className="p-4 space-y-2 text-sm dark:text-gray-400">
         <p>{reviewText}</p>
         <div className="flex items-center space-x-4 mt-2 cursor-pointer">
-          <span>
+          <span className="p-2 bg-white text-black" onClick={() => setCardButton(!cardButton)}>
             <FaRegEdit></FaRegEdit>
           </span>
-          <span onClick={() => handleDelete(_id)}>
+          <span className="p-2 bg-white text-black" onClick={() => handleDelete(_id)}>
             <FaArchive></FaArchive>
           </span>
         </div>
       </div>
+      {
+        cardButton && <Modal cardButton={cardButton} setCardButton={setCardButton} handleUpdate={handleUpdate} serviceName={serviceName}></Modal>
+      }
+      
     </div>
   );
 };
