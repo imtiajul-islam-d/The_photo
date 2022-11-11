cefea6ea28eefc359f7890060b53a87cfde43e4d
imtiajul-islam-d <imtiajul@hotmail.com>
2022-11-11 10:56:17 +0600

    correction on review page

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -49,10 +49,7 @@ const MyReviews = () => {
     <section className="py-6 sm:py-12 dark:bg-gray-800 dark:text-gray-100 container mx-auto">
       <div className="container p-6 mx-auto space-y-8">
         <div className="space-y-2 text-center">
-          <h2 className="text-3xl font-bold">Partem reprimique an pro</h2>
-          <p className="font-serif text-sm dark:text-gray-400">
-            Qualisque erroribus usu at, duo te agam soluta mucius.
-          </p>
+          <h2 className="text-3xl font-bold">The reviews that you have given in several services</h2>
         </div>
 
         {loader && (
